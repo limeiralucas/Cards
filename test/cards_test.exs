8b7d720033f17a7214142faa1a10738a227277dd
Lucas Limeira <lucasalveslm@gmail.com>
2019-01-26 15:30:06 -0300

    class 29

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,14 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 52 cards" do
+      deck_lengh = length(Cards.create_deck)
+      assert deck_lengh == 52
+  end
+
+  test "shuffling a deck randomizes it" do
+      deck = Cards.create_deck
+      refute deck == Cards.shuffle deck
+  end
 end
